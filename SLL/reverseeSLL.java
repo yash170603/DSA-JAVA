eac56d9da1bce47c8afc186eae3525082a1e594f
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-07-19 16:19:59 +0530

    reverseeSLL.java

--- SLL/reverseeSLL.java
+++ SLL/reverseeSLL.java
@@ -1,1 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode nnode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return nnode;
+    }
+}
 
