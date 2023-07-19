4ef7c0bcc8fe29cdc116100d374f1c00efef1e19
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-07-19 20:50:35 +0530

    detetctloop2.java

--- SLL/detetctloop2.java
+++ SLL/detetctloop2.java
@@ -17,7 +17,7 @@ public class Solution {
             return false;
         }
 
-         ListNode slow = head ,fast = head;
+         ListNode slow = head ,fast = head.next;
         while(fast!=null && fast.next!=null) {
             slow = slow.next;
             fast = fast.next.next;
