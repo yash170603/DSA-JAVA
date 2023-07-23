d91e11820329d9cc819484189393af0422e7b1fb
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-07-23 10:37:17 +0530

    detectstartpoint.java

--- SLL/detectstartpoint.java
+++ SLL/detectstartpoint.java
@@ -33,3 +33,28 @@ public class detectstartpoint {
         return tortoise;
     }
 }
+
+
+
+/*public class Solution {    this is hashset method
+    public ListNode detectCycle(ListNode head) {
+        // Initialize an empty hash set
+        HashSet<ListNode> nodesSeen = new HashSet<>();
+        
+        // Start from the head of the linked list
+        ListNode node = head;
+        while (node != null) {
+            // If the current node is in nodesSeen, we have a cycle
+            if (nodesSeen.contains(node)) {
+                return node;
+            } else {
+                // Add this node to nodesSeen and move to the next node
+                nodesSeen.add(node);
+                node = node.next;
+            }
+        }
+
+        // If we reach a null node, there is no cycle
+        return null;
+    }
+}*/
