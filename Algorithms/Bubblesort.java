0693f7de995e2c2344de46120dd87ec378d2ea2d
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-07-24 15:43:28 +0530

    Bubblesort.java

---
+++ Algorithms/Bubblesort.java
@@ -0,0 +1,22 @@
+  public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            
+            for (int j = 0; j < n - i - 1; j++) {
+                // If the current element is greater than the next element, swap them
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            // If no two elements were swapped in the inner loop, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
