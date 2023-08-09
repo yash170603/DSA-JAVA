6cb2edeafba5671aea5fcc039376ad132c85ceaa
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-08-09 19:47:34 +0530

    Create reversearray.java

---
+++ RECURSION/reversearray.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class reversearrayrecursion 
+{
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner (System.in);
+        int a [] = {1,2,3,4,5};
+        int v = a.length -1;
+        int y =0;
+        reversearray(a, y,v );
+        for(int x : a)
+        {
+            System.out.print(x);
+        }
+
+    }
+
+    public static void reversearray(int []a, int y , int v)
+    {
+        int temp = a[y];
+        a[y] = a[v];
+        a[v] = temp;
+        if(v == y)
+        return;
+
+        reversearray(a, y+1, v-1);
+     }
+
+
+
+
+
+
+
+
+}
