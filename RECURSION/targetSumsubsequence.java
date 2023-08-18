098b762321b304a7c7c8c77e6510da80f9a6c810
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-08-18 15:09:13 +0530

    Update targetSumsubsequence.java

--- RECURSION/targetSumsubsequence.java
+++ RECURSION/targetSumsubsequence.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class jj
- {
+ {   //prints all subseqquences
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
@@ -39,6 +39,62 @@ public class jj
         x.remove(x.size()-1);
         sum -= arr[i];
         prints(arr,  x, i+1, sum, target);
+
+
+   }
+}
+
+
+
+//prints only one subsequence
+import java.util.*;
+public class sumsequence
+ {            //gives only 1 subsequence
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int arr [] ={3,2,1,4,6};
+        
+        int  i =0;
+        int sum= 0;
+        int target = 4;
+        ArrayList<Integer> x = new ArrayList<>();
+        prints(arr,x,i,sum,target);
+
+    }
+ 
+   public static boolean prints(int [] arr,  ArrayList<Integer> x, int i, int sum, int target)
+   {
+
+       if(i == arr.length)
+       {
+        if( sum == target)
+        {
+           for(int j =0 ;j<x.size();j++)
+        {
+            System.out.print(x.get(j));
+            System.out.print(" ");
+        }
+       return true;
+        }
+      
+        return false ;
+       }
+
+        x.add(arr[i]);
+        sum +=  arr[i];
+       if(prints(arr,  x, i+1, sum, target) == true) 
+       return true;
+        x.remove(x.size()-1);
+        sum -= arr[i];
+        if(prints(arr,  x, i+1, sum, target) == true) 
+        return true;
+
+
+
+
+        return false;
 
 
    }
