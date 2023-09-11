d0dad18cee56d2fd8459673e1365b7a413900184
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-09-11 22:14:48 +0530

    findintersection.java

--- SLL/findintersection.java
+++ SLL/findintersection.java
@@ -2,21 +2,51 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         //we will create a hashmap and put all the list1 values into that map lets do it.
-       /* HashSet<ListNode> map=new HashSet<ListNode>();
-        while(headA!=null){
-            map.add(headA);
-            headA=headA.next;
-        }
-        // we have successfully put all the values of linked list1 into map;
-        //no we will just check
-        while(headB!=null){
-            if(map.contains(headB))
-            return headB;
-            headB=headB.next;
-        }
-        return null;*/
+      public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB)
+     {
+        
+     ListNode c1 =headA;
+     ListNode c2 = headB;
+     ListNode result = null;
+      
+      HashSet<ListNode> x = new HashSet<>();
+
+      if(c1 ==  c2)
+      return c1;
+    //   if( c1.next == null && c2.next == null)
+    //   {
+    //       return result;
+    //   }
+     
+      
+      while(c2 != null )
+      {
+          x.add(c2);
+          c2 = c2.next;
+      }
+
+  
+      while(c1 != null )
+      {
+          if(x.contains(c1))
+          {
+              result = c1;
+              break;
+          }
+
+          c1=c1.next;
+      }
+
+
+     return result;
+
+
+
+    }
+}
         // efficient solution
-       /* ListNode dummy=headA;
+        ListNode dummy=headA;
         ListNode dummy1=headB;
         int l1=0,l2=0;
         while(headA!=null){
@@ -48,7 +78,7 @@ public class Solution {
             dummy=dummy.next;
             dummy1=dummy1.next;
         }
-        return null;*/
+        return null;
         /*The intuition behind the approach of finding the intersection point of two linked lists by adjusting their starting positions
         based on the difference in lengths is to ensure that both lists have the same number of nodes to traverse before reaching the intersection point.
         The key observation is that if two linked lists intersect at a certain node, all the subsequent nodes will be common in both lists.
