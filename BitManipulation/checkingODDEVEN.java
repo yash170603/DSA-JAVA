5b43ca7fb6b1f6bb899efdcccfed91d9fef1778d
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-09-12 19:33:19 +0530

    checkingODDEVEN.java

---
+++ BitManipulation/checkingODDEVEN.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static String oddEven(int N){
+       
+
+
+       if((N|1) == N)
+       return "odd";
+
+       else
+       return"even";
+       
+       
+    }
+}
