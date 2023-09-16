46b4429239387da0a396cef04608c7371bfafe18
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-09-16 11:38:56 +0530

    Create QeuesAsArrays.java

---
+++ Stacks/QeuesAsArrays.java
@@ -0,0 +1,29 @@
+public class Solution {
+    class Queue {
+        int front, rear;
+        int []arr;
+
+        Queue() {
+            front = 0;
+            rear = 0;
+            arr = new int[100001];
+        }
+
+        // Enqueue (add) element 'e' at the end of the queue.
+        public void enqueue(int e) {
+            arr[front] = e;
+            front+=1;
+        }
+
+        // Dequeue (retrieve) the element from the front of the queue.
+        public int dequeue() {
+            if(rear >= front )
+            return -1;
+            int x= arr[rear];
+           
+            rear = rear+1;
+             return x;
+           
+        }
+    }
+}
