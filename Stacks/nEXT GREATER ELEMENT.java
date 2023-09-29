628a0f37e8498d8283e656153ec7462ec831010c
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-09-29 22:08:29 +0530

    Update nEXT GREATER ELEMENT.java

--- Stacks/nEXT GREATER ELEMENT.java
+++ Stacks/nEXT GREATER ELEMENT.java
@@ -43,7 +43,49 @@ class Solution {
     }
 }
 
+// from back word,, my pref 
+//starting with the end of array, when stack is empty, there is no greatter element the the last so put -1 in map
+//push it in stack. Now next element compare it with top element in stack. if nums2 < topof stack
+//put stack.peek in map else we will keep popping until we find element in stack which is greater than nums2 element
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // int [] arr = new int[nums2.length];
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = nums2.length-1; i>=0; i--){
+            
+            if(stack.isEmpty()) {
+                // arr[i] = -1;
+                map.put(nums2[i], -1);
+            }
+            else if(!stack.isEmpty() && nums2[i]< stack.peek()){
+                // arr[i]= stack.peek();
+                map.put(nums2[i], stack.peek());
+            }
+            else{ // (!stack.isEmpty() && nums2[i]>= stack.peek())
 
+                while(!stack.isEmpty() && nums2[i]>= stack.peek()){
+                    stack.pop();
+                }
+                    if(stack.isEmpty()){
+                        // arr[i]= -1;
+                        map.put(nums2[i], -1);
+                    }
+                    else{
+                        // arr[i] = stack.peek();
+                        map.put(nums2[i], stack.peek());
+                    }    
+            }
+            stack.push(nums2[i]);
+        }
+
+        int ans[] = new int[nums1.length];
+        for(int i=0; i<nums1.length; i++){
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
+    }
+}
 
 
 
@@ -53,69 +95,40 @@ class Solution {
 //2nd for 1 array oonly
 
 
-import java.util.Stack;
-import java.util.HashMap;
-public class Solution {
-    public static int[] nextGreaterElement(int[] arr, int n)
-     {
-      Stack<Integer> s1 = new Stack<Integer>();
-    HashMap<Integer,Integer> x = new HashMap<Integer,Integer>();
-    
-    int ans[] = new int [n];
-    
 
-    for(int i= n-1;i>=0;i--)
-    {
-        while(s1.isEmpty()!= true && s1.peek()<= arr[i])
-        {
-            s1.pop();
-            
+  int[] ans = new int[n];
+
+         // Stack, to keep track of next greater element
+         Stack<Integer> s = new Stack<Integer>();
+
+         // Traverse in reverse fashion in the array
+         for(int i = n - 1; i >= 0; i--)
+         {
+             // If current element is greater than or equal to stack's top 
+             // element, we can pop it since now it will never get picked
+             // as the next greater element
+             while(!s.empty() && arr[i] >= s.peek())
+             {
+                  s.pop();
+             }
+
+             // If stack is not empty, stack's top element is 
+             // the next greater element, else there is none
+             if(!s.empty())
+             {
+               ans[i] = s.peek();
+           }
+           else
+           {
+               ans[i] = -1;
+           }
+
+           // Push current element onto stack
+            s.push(arr[i]);
         }
-x.put(arr[i], s1.isEmpty() ? -1 : s1.peek());
-       
-        s1.push(arr[i]);
-        
-    }
 
-    for(int h=0;h<ans.length;h++)
-    {
-        ans[h] = x.getOrDefault(arr[h]);
-    }
-
-    return ans;
-//  int[] ans = new int[n];
-
-//         // Stack, to keep track of next greater element
-//         Stack<Integer> s = new Stack<Integer>();
-
-//         // Traverse in reverse fashion in the array
-//         for(int i = n - 1; i >= 0; i--)
-//         {
-//             // If current element is greater than or equal to stack's top 
-//             // element, we can pop it since now it will never get picked
-//             // as the next greater element
-//             while(!s.empty() && arr[i] >= s.peek())
-//             {
-//                  s.pop();
-//             }
-
-//             // If stack is not empty, stack's top element is 
-//             // the next greater element, else there is none
-//             if(!s.empty())
-//             {
-//                 ans[i] = s.peek();
-//             }
-//             else
-//             {
-//                 ans[i] = -1;
-//             }
-
-//             // Push current element onto stack
-//             s.push(arr[i]);
-//         }
-
-//         // return final answer vector
-//         return ans;
+        // return final answer vector
+        return ans;
       
     }
 }
