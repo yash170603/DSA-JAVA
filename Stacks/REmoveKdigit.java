773c3f0d1decc7ca39765be0e8daa6fcc43c7e23
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-10-25 22:47:57 +0530

    Create REmoveKdigit.java

---
+++ Stacks/REmoveKdigit.java
@@ -0,0 +1,74 @@
+// class Solution {
+//     public String removeKdigits(String num, int k) {
+//         Stack<Character> st = new Stack<>();
+//         for(Character ch : num.toCharArray()) {
+//             while(!st.isEmpty() && k > 0 && ch < st.peek()) {
+//                 st.pop();
+//                 k--;
+//             }
+
+//             if(!st.isEmpty() || ch != '0') {
+//                 st.push(ch);
+//             }
+//         }
+
+//         // This is a special edge case --> 1 2 3 4
+//         while(!st.isEmpty() && k-- > 0) {
+//             st.pop();
+//         }
+//         if(st.isEmpty()) return "0";
+        
+
+//         // Store the ans -->
+//         String ans = "";
+//         while(!st.isEmpty()) {
+//             ans = st.peek() + ans;
+//             st.pop();
+//         }
+//         return ans;
+//     }
+// }
+class Solution {
+    public String removeKdigits(String num, int k) 
+    {
+        if(k == num.length())
+            return "0";
+        
+        Stack<Character> stack = new Stack<>();
+        
+        int i=0;
+        
+        while(i < num.length())
+        {
+            char ch = num.charAt(i);
+            //whenever meet a digit which is less than the previous digit, discard the previous one
+            while(!stack.isEmpty() && stack.peek() > ch && k > 0)
+            {
+                stack.pop();
+                k--;
+            }
+            stack.push(ch);
+            i++;
+        }
+        //for corne case like 1111
+        while(k > 0)
+        {
+            stack.pop();
+            k--;
+         }
+        
+        //constructing number from stack
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(!stack.isEmpty())
+            sb.append(stack.pop());
+        
+        sb.reverse();
+        
+        while(sb.charAt(0) == '0' && sb.length() > 1)
+            sb.deleteCharAt(0);
+        
+        return sb.toString();
+    }
+}
