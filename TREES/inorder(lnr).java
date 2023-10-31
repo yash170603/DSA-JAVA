e2b90e40e60a7e30cf1c0954629c74995397a42b
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-10-31 20:28:55 +0530

    Update inorder(lnr).java\

--- TREES/inorder(lnr).java
+++ TREES/inorder(lnr).java
@@ -20,3 +20,40 @@ class Solution {
         inorderRecursive(node.right, result);
     }
 }
+
+//Stack Approach
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root)
+     {
+         List<Integer> result = new ArrayList<>();
+         Stack<TreeNode> s1 = new Stack<TreeNode>();
+        inorder(root, result,s1);
+        return result;
+    }
+    public void inorder(TreeNode root, List<Integer> result, Stack<TreeNode> s1)
+    {
+          TreeNode node = root;
+
+          while(true)
+          {
+              if(node != null)
+              {
+                  s1.push(node);
+                  node = node.left;
+              }
+
+              else
+              {
+                  if(s1.isEmpty() == true)
+                  {
+                      break;
+                  }
+
+                  node = s1.pop();
+                  result.add(node.val);
+                  node = node.right;
+              }
+          }
+        
+    }
+}
