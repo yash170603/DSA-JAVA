0294127aa3b73e73214abf29d54f11aa33b944bb
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-11-01 20:36:06 +0530

    Update postOrder(lrn).java

--- TREES/postOrder(lrn).java
+++ TREES/postOrder(lrn).java
@@ -38,3 +38,63 @@ class Solution {
         result.add(node.val);
     }
 }
+// using 2 Stack
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> postOrder = new ArrayList<>();
+        if (root == null)
+            return postOrder;
+
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        s1.push(root);
+
+        while (!s1.isEmpty()) {
+            TreeNode curr = s1.pop();
+            s2.push(curr);
+
+            if (curr.left != null)
+                s1.push(curr.left);
+
+            if (curr.right != null)
+                s1.push(curr.right);
+        }
+
+        while (!s2.isEmpty()) {
+            postOrder.add(s2.pop().val);
+        }
+
+        return postOrder;
+    }
+}
+
+// using 2 stack
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> postOrder = new ArrayList<>();
+        if (root == null)
+            return postOrder;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        TreeNode lastVisited = null;
+
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode temp = stack.peek();
+                if (temp.right != null && temp.right != lastVisited) {
+                    cur = temp.right;
+                } else {
+                    stack.pop();
+                    postOrder.add(temp.val);
+                    lastVisited = temp;
+                }
+            }
+        }
+        return postOrder;
+    }
+}
+
