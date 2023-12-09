45dfabd2dacfd1f52e4436cd46aac86336ef7d81
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-12-09 16:56:59 +0530

    Update TreeFomINOrderndPREorder.java

--- TREES/TreeFomINOrderndPREorder.java
+++ TREES/TreeFomINOrderndPREorder.java
@@ -61,3 +61,36 @@ class Solution {
     return root;
   }
 }
+
+
+
+
+
+
+
+//2nd 
+
+class Solution {
+    private int preorderIdx = 0;
+    private TreeNode construct(int[] preorder, HashMap<Integer,Integer> map, int left, int right){
+        if(left>right) return null;
+        int pval = preorder[preorderIdx];
+        int inorderIdx = map.get(pval);
+
+        TreeNode root = new TreeNode(pval);
+        preorderIdx++;
+        root.left = construct(preorder, map, left, inorderIdx-1);
+        root.right = construct(preorder, map, inorderIdx+1, right);
+        return root;
+        
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int len = inorder.length;
+        for(int i=0;i<len;i++){
+            map.put(inorder[i],i);
+        }
+
+        return construct(preorder, map, 0, len-1);
+    }
+}
