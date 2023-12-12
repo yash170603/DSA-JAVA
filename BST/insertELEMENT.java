82c4704804cabd376a759b89bf725cb702b4d8f4
yash170603 <125726917+yash170603@users.noreply.github.com>
2023-12-12 11:06:36 +0530

    Update insertELEMENT.java

--- BST/insertELEMENT.java
+++ BST/insertELEMENT.java
@@ -71,3 +71,81 @@ class Solution {
 
 // The recursion goes down the tree until it finds the appropriate position to insert the new node.
 // The worst-case time complexity is O(n) when the tree is unbalanced, and the best-case time complexity is O(log n) when the tree is balanced.
+
+
+//deletion
+
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null)
+            return null;
+
+        return solve(root, key);
+    }
+
+    public TreeNode solve(TreeNode root, int k) {
+        if (root == null)
+            return null;
+
+        if (root.val == k) {
+            if (root.left == null && root.right == null) {
+                return null; // Node with no children
+            } else if (root.left == null) {
+                return root.right; // Node with only right child
+            } else if (root.right == null) {
+                return root.left; // Node with only left child
+            } else {
+                // Node with both left and right children
+                root.val = minValue(root.right);
+                root.right = solve(root.right, root.val);
+                return root;
+            }
+        }
+
+        if (root.val < k) {
+            root.right = solve(root.right, k);
+        } else {
+            root.left = solve(root.left, k);
+        }
+
+        return root;
+    }
+
+    public int minValue(TreeNode root) {
+        if (root == null) {
+            return -1; // Handle the case where the tree is empty
+        }
+
+        while (root.left != null) {
+            root = root.left;
+        }
+
+        int x = root.val;
+        return x;
+    }
+
+    // Additional methods...
+}
+
+/*The time complexity of your `deleteNode` operation in a Binary Search Tree (BST) depends on the height of the tree. In the worst case,
+the height of the tree can be \(O(n)\), where \(n\) is the number of nodes in the tree.
+
+Here's a breakdown of the time complexity for various cases:
+
+1. **Deleting a Leaf Node:**
+   - If the node to be deleted is a leaf (has no children), the operation takes constant time \(O(1)\) because you simply remove the node.
+
+2. **Deleting a Node with One Child:**
+   - If the node has one child, you replace the node with its child, and this operation also takes constant time \(O(1)\).
+
+3. **Deleting a Node with Two Children:**
+   - If the node to be deleted has two children, your implementation replaces the node's value with the minimum value
+   from its right subtree and then recursively deletes the node containing the minimum value in the right subtree.
+   This operation takes \(O(h)\) time, where \(h\) is the height of the tree.
+
+4. **Worst Case:**
+   - If the tree is unbalanced, and the height is \(O(n)\), the overall time complexity for the `deleteNode` operation is \(O(n)\).
+
+To improve the average-case time complexity, consider using a self-balancing Binary Search Tree (BST), such as an AVL tree or a Red-Black tree.
+These trees maintain balance during insertion and deletion operations, ensuring that the height of the tree remains \(O(\log n)\), 
+leading to \(O(\log n)\) time complexity for deletion operations on average.*/
