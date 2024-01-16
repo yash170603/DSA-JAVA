1deb7b9c576a8a07ff7f5598e70d368ee91007d0
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-01-16 23:41:01 +0530

    Update Minimum Number of Steps to Make Two Strings Anagram.java

--- Daily challenge/Minimum Number of Steps to Make Two Strings Anagram.java
+++ Daily challenge/Minimum Number of Steps to Make Two Strings Anagram.java
@@ -1,26 +1,31 @@
 class Solution {
-    public int minSteps(String s, String t) {
-        Map<Character,Integer> map = new HashMap<Character,Integer>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch, map.get(ch)+1);
-            }
-            else{
-                map.put(ch,1);
+    public boolean closeStrings(String word1, String word2) {
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (char ch : word1.toCharArray()) {
+            freq1[ch - 'a']++;
+        }
+
+        for (char ch : word2.toCharArray()) {
+            freq2[ch - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
             }
         }
-        int count=0;
-        for(int i = 0;i<t.length();i++){
-            char ch=t.charAt(i);
-            if(map.containsKey(ch) && map.get(ch) > 0){
-                map.put(ch, map.get(ch) - 1);
-            }
-            else{
-                count++;
+
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
             }
         }
-        return count;
-        
+
+        return true;
     }
 }
