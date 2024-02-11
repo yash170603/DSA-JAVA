8d89db67ecbe58cd3f0ba79aca3cec8698098fc6
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-02-11 13:29:57 +0530

    Update climbingStairs.java

--- DP/climbingStairs.java
+++ DP/climbingStairs.java
@@ -1,49 +1,47 @@
+class Solution {
+    public int rob(int[] nums)
+     {
+         int t[]= new int [101];
+         t[0]=0;
+         t[1]=nums[0];
+         for( int i =2;i<=nums.length;i++)
+         {
+             int steal = nums[i-1]+t[i-2];
+             int skip = t[i-1];
+             t[i] = Math.max(steal,skip);
+         }
+
+         return t[nums.length];
+    }
+
+}
+
+// 2nd approach
 import java.util.Arrays;
 
-// Approach-1 (Recursion with Memo) : Top Down
-class Solution {
-    int[] t = new int[46];
+class Solution {    //recursion+memoization
+    int[] t = new int[101];
 
-    public int solve(int n) {
-        if (n == 0 || n == 1 || n == 2)
-            return n;
+    public int solve(int[] nums, int i, int n) {
+        if (i >= n)
+            return 0;
 
-        if (t[n] != -1)
-            return t[n];
+        if (t[i] != -1)
+            return t[i];
 
-        int a = solve(n - 1);
-        int b = solve(n - 2);
+        int take = nums[i] + solve(nums, i + 2, n); // steals ith house and moves to i+2 (because we can't steal adjacent)
+        int skip = solve(nums, i + 1, n); // skips this house, now we can move to adjacent next house
 
-        return t[n] = a + b;
-
+        return t[i] = Math.max(take, skip);
     }
 
-    public int climbStairs(int n) {
+    public int rob(int[] nums) {
+        int n = nums.length;
         Arrays.fill(t, -1);
-        return solve(n);
+        return solve(nums, 0, n);
     }
 }
 
-// Approach-2 (Using Bottom Up DP)
-class Solution2 {
-    public int climbStairs(int n) {
-        if (n == 1 || n == 2)
-            return n;
-
-        int[] t = new int[n + 1];
-
-        // t[i] = total ways to climb i stairs
-        t[0] = 0;
-        t[1] = 1;
-        t[2] = 2;
-
-        for (int i = 3; i <= n; i++) {
-            t[i] = t[i - 1] + t[i - 2];
-        }
-
-        return t[n];
-    }
-}
 
 // Approach-3 (Improving approach-2)
 // See that it looks nothing but a fibonacci sequence. You just have to store last 2
