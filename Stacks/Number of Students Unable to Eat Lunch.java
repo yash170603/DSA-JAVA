2d7a5a301bfeb847cf5279b4c5ff20f536b4d9a4
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-04-08 11:06:00 +0530

    Update Number of Students Unable to Eat Lunch.java

--- Stacks/Number of Students Unable to Eat Lunch.java
+++ Stacks/Number of Students Unable to Eat Lunch.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countStudents(int[] student, int[] sand) {
-        
-
+         // here t.c may look like o(N) but no not true, its almost >= o(n2)
+       // ek element hatne k baad new ques poori traverse hori agar element last mei hai
         Stack<Integer> s = new Stack<Integer>();
         Queue<Integer> q = new LinkedList<>();
 
@@ -29,10 +29,34 @@ class Solution {
                 int d = q.peek();
                 q.poll();
                 q.offer(d);
-                lastnotserved++;
+                lastnotserved++;  //  this is the main logic, check how may students were unluck before this mf got luck and got his sandwhich
             }
         }
 
         return lastnotserved;
     }
 }
+
+
+// 2nd approach
+//kind of greedy , agar q mei koi aisa hai jisko pehla sandwhich chaiye toh vo le jaega. something like thsi
+
+public class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int zeroCount = 0, oneCount = 0;
+
+        for (int student : students) {
+            if (student == 0) zeroCount++;
+            else oneCount++;
+        }
+
+        for (int sandwich : sandwiches) {
+            if (sandwich == 0 && zeroCount == 0) return oneCount;
+            if (sandwich == 1 && oneCount == 0) return zeroCount;
+            
+            if (sandwich == 0) zeroCount--;
+            else oneCount--;
+        }
+
+        return 0;
+    }
