e776873322c871bc0f501225b4eadd506a53d46c
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-05-18 15:15:02 +0530

    Update Minimum Insertion Steps to Make a String Palindrome(28).java

--- DP/Minimum Insertion Steps to Make a String Palindrome(28).java
+++ DP/Minimum Insertion Steps to Make a String Palindrome(28).java
@@ -30,3 +30,27 @@ class Solution {
         return dp[indexi][indexj] = 0+ solve( s, indexi+1,indexj-1);
     }
 }
+
+
+//bottom up
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        
+        // Fill the table from the bottom up
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[0][n - 1];
+    }
+}
+
