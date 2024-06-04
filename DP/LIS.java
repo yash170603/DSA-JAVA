6988417c18a70c315b4f94b0be65862af7e03a75
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-06-04 11:41:23 +0530

    Update LIS.java

--- DP/LIS.java
+++ DP/LIS.java
@@ -67,3 +67,49 @@ class Solution {
         return maxLIS;
     }
 }
+
+
+
+
+// lis using binary search
+
+    import java.util.*;
+
+class LIS {
+    // Function to find the length of the longest increasing subsequence
+    static int longestIncreasingSubsequence(int[] arr, int n) {
+        List<Integer> temp = new ArrayList<>();
+        temp.add(arr[0]);
+
+        int len = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > temp.get(temp.size() - 1)) {
+                // arr[i] > the last element of temp array
+
+                temp.add(arr[i]);
+                len++;
+            } else {
+                // Replacement step
+                int ind = Collections.binarySearch(temp, arr[i]);
+
+                if (ind < 0) {
+                    ind = -ind - 1;
+                }
+
+                temp.set(ind, arr[i]);
+            }
+        }
+
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 2, 5, 3, 7, 101, 18};
+        int n = arr.length;
+
+        System.out.println("The length of the longest increasing subsequence is " +
+                longestIncreasingSubsequence(arr, n));
+    }
+}
+
