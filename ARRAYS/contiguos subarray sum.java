d056fb904ace0f907942cee6c6f2758ed3b0a28f
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-06-08 11:31:26 +0530

    Create contiguos subarray sum.java

---
+++ ARRAYS/contiguos subarray sum.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        mp.put(0, -1);
+        
+        int sum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            
+            int remainder = sum % k;
+            
+            if (mp.containsKey(remainder)) {
+                if (i - mp.get(remainder) >= 2)
+                    return true;
+            } else {
+                mp.put(remainder, i);
+            }
+        }
+        return false;
+    }
+}
