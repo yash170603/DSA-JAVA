3bcfd8abf96eed54aa167c86f09b6eda40c1ed76
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-06-15 23:00:37 +0530

    Create split Array(9).java

---
+++ Binary Search/split Array(9).java
@@ -0,0 +1,39 @@
+class Solution {
+    public int splitArray(int[] a, int k) {
+        int low = a[0];
+        int high = 0;
+        //find maximum and summation:
+        for (int i = 0; i < a.length; i++) {
+            low = Math.max(low, a[i]);
+            high += a[i];
+        }
+
+        //Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int partitions = countPartitions(a, mid);
+            if (partitions > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    public int countPartitions(int[] a, int maxSum) {
+        int n = a.length; //size of array.
+        int partitions = 1;
+        long subarraySum = 0;
+        for (int i = 0; i < n; i++) {
+            if (subarraySum + a[i] <= maxSum) {
+                //insert element to current subarray
+                subarraySum += a[i];
+            } else {
+                //insert element to next subarray
+                partitions++;
+                subarraySum = a[i];
+            }
+        }
+        return partitions;
+    }
+}
