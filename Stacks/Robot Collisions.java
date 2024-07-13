d86e05eeeed41fb63c79c1796b7cbf8fadc14e55
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-07-13 14:54:47 +0530

    Update Robot Collisions.java

--- Stacks/Robot Collisions.java
+++ Stacks/Robot Collisions.java
@@ -52,3 +52,83 @@ public class Solution {
 
     
 }
+
+
+
+
+// this passed 350/2344
+
+class Solution {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        
+        Stack<int[]> s = new Stack<>();
+        List<int[]> x = new ArrayList<>(); // right ==1, left ==0;
+        int n = positions.length;
+        for( int i = 1;i<=n;i++)
+        {
+            x.add( new int []{positions[i-1],healths[i-1],(directions.charAt(i-1)=='R')?1:0,i});
+        }
+        Collections.sort(x, (a,b)->a[0]-b[0]);
+      //  List<Integer>ans= new ArrayList<>();
+
+        for( int arr [] : x)
+        {
+            if( s.isEmpty() || s.peek()[2] == arr[2] || (s.peek()[2] ==0 && arr[2] ==1)  )  // same direction   OR last element goin left, coming goes right
+            {
+                s.push(arr);
+            }
+            else   // directions different hai, R->  <-L
+            {
+                     while( !s.isEmpty() &&  (s.peek()[2] == 1 && arr[2]==0)) {   // R-> R->  <-L
+                             if( s.peek()[1]>arr[1])
+                             {
+                                  int temp []= s.pop();
+                                  temp[1]=temp[1]-1;
+                                 s.push(temp);
+                                 break;
+                             }
+                             if( s.peek()[1]<arr[1])
+                             {
+                                s.pop();
+                                s.push( new int[]{ arr[0],arr[1]-1,arr[2],arr[3]});
+                             }
+                             if( s.peek()[1] == arr[1])
+                             {
+                                s.pop();
+                                break;
+                             }
+                     }                                     
+
+
+            }
+        }
+        List<int[]> d= new ArrayList<>();
+        while( !s.isEmpty())
+        {
+            d.add(s.pop());
+        }
+        Collections.sort(d, (a,b)->a[3]-b[3]);
+
+        List<Integer> ans = new ArrayList<>();
+        for( int p[]:d)
+        {
+            ans.add(p[1]);
+        }
+
+        return ans;
+    }
+}
+
+
+fails this positions =
+[11,44,16]
+healths =
+[1,20,17]
+directions =
+"RLR"
+
+Use Testcase
+Output
+[1,19]
+Expected
+[18]
