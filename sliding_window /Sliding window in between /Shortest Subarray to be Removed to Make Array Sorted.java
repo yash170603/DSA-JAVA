97ac005453e6da3f9486ea99b536596796832047
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-11-15 19:39:35 +0530

    Update Shortest Subarray to be Removed to Make Array Sorted.java

--- sliding_window /Sliding window in between /Shortest Subarray to be Removed to Make Array Sorted.java
+++ sliding_window /Sliding window in between /Shortest Subarray to be Removed to Make Array Sorted.java
@@ -3,7 +3,7 @@ class Solution {
            int n=nums.length;
            int j=n-1;
            while( j>0 && nums[j]>=nums[j-1]) j--;
-           int ans=j;
+           int ans=j;    // from j to left(inclusive) everything is sorted.
            if( j==0) return 0;
            int i=0;
            while( i<j && (i==0 || nums[i]>=nums[i-1])){
