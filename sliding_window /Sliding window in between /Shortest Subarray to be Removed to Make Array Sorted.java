e8536be640324fa5e4bb745e5aa82e7c50c57440
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-11-15 19:38:48 +0530

    Create  Shortest Subarray to be Removed to Make Array Sorted.java

---
+++ sliding_window /Sliding window in between /Shortest Subarray to be Removed to Make Array Sorted.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLengthOfShortestSubarray(int[] nums) {
+           int n=nums.length;
+           int j=n-1;
+           while( j>0 && nums[j]>=nums[j-1]) j--;
+           int ans=j;
+           if( j==0) return 0;
+           int i=0;
+           while( i<j && (i==0 || nums[i]>=nums[i-1])){
+               while( j<n && nums[i]>nums[j]){
+                j++;
+               }
+               ans=Math.min( ans, j-i-1);
+               i++;
+           }
+      return ans;
+    }
+}
