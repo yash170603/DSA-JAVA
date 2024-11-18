bcb52f94c15e651384842014e8ae392b7711c089
yash170603 <125726917+yash170603@users.noreply.github.com>
2024-11-19 02:53:26 +0530

    Create K-th Symbol in Grammar.java

---
+++ String/Strings+Recursion(kth character bit in string or number)/K-th Symbol in Grammar.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if(n==1) return 0;
+          
+          int length =(int) Math.pow(2,n-1);
+          int mid= length/2;
+
+          if(k<=mid) return kthGrammar(n-1,k);
+
+          return kthGrammar(n-1,k-mid)==0?1:0;
+    }
+}
